e75b638cef27eea11c2f54db0d35349041c8ce6b
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-09-04 00:42:19 +0530

    Error Handlers Added

--- blogify/routes.py
+++ blogify/routes.py
@@ -2,6 +2,21 @@ from flask import Flask,render_template
 from blogify import app
 
 @app.route('/')
-@app.route('/landing')
+@app.route('/home')
 def home():
-    return "<p>Hello World</p>"
+    return render_template('home.html')
+
+@app.route('/user/<name>')
+def user(name):
+    favourite_food=['pizza','burger']
+    return render_template('user.html',name=name,foods=favourite_food)
+
+# Page Not Found
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+#Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
