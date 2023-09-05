fe5190bf719aa9ebb341b254286418379c6fdb91
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-09-05 19:03:30 +0530

    SQLAlchemy

--- blogify/__init__.py
+++ blogify/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False  
 app.config['SECRET_KEY']='08731aa961f88a8835c70351'
+db = SQLAlchemy(app)
 
 from blogify import routes
