e93db4c76130c94da316620d89eb21d9303ce329
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-09-19 11:21:47 +0530

    .env hidden

--- blogify/__init__.py
+++ blogify/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_ckeditor import CKEditor
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app = Flask(__name__)
 
 # OLD SQLite3 DB
@@ -11,7 +13,7 @@ app = Flask(__name__)
 
 # NEW MySQL DB
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:rampage@localhost/users'
-app.config['SECRET_KEY']='08731aa961f88a8835c70351'
+app.config['SECRET_KEY']=os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.init_app(app)
